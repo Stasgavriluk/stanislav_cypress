86ea0b45b1bdffc287fee14bc2b5540362968f2d
stanislav <stasgavrilyk22@gmail.com>
2022-07-16 11:46:43 +0300

    homework

---
+++ cypress/selectors/sign_in_page.js
@@ -0,0 +1,13 @@
+export const sign_in_page = {
+    logo_image: '.makeStyles-logo-3',
+    title_text: '.MuiTypography-h5',
+    username_field: '#username',
+    password_field: '#password',
+    username_placeholders: '#username-label',
+    password_placeholders: '#password-label',
+    username_required: '.MuiFormHelperText-root',
+    checkbox: '.PrivateSwitchBase-input-14',
+    button: "[data-test='signin-submit']",
+    link: "[data-test='signup']",
+    copyright: "[rel='noopener noreferrer']"
+}
