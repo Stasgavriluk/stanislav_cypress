2910215c27890caffada0170353466bc8324b8b6
stanislav <stasgavrilyk22@gmail.com>
2022-07-25 18:09:02 +0300

    fixed commands.js (hw3)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,11 @@
 import {sign_in_page, user_info} from "../selectors/sign_in_page";
 import {main_page} from "../selectors/main_page.selector";
 
-Cypress.Commands.add("ui_login", (username, password, { rememberUser = false } = {}) => {
+Cypress.Commands.add("ui_login", () => {
     cy.visit("/signin")
     cy.intercept("POST", "/login").as("loginUser")
-
-    cy.get(sign_in_page.username_field).type(username)
-    cy.get(sign_in_page.password_field).type(password)
-
-    if (rememberUser) {
-        cy.get(sign_in_page.checkbox).find("input").check()
-    }
-
+    cy.get(sign_in_page.signup_username).type(user_info.username)
+    cy.get(sign_in_page.signup_password).type(user_info.password)
     cy.get(sign_in_page.signin_submit).click();
     cy.wait("@loginUser")
 })
